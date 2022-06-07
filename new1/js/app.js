981a50231fde34fca6cfc90ff6d9fd28240c97ca
Sarkhan666 <101628030+Sarkhan666@users.noreply.github.com>
2022-06-08 00:18:20 +0400

    Add files via upload

---
+++ new1/js/app.js
@@ -0,0 +1,35 @@
+//task1
+/*var hour = new Date().getHours();
+console.log(hour);
+if(hour>9&&hour<=13)
+{
+    document.body.style.backgroundColor='red';
+    alert('salam dostlaaaar');
+}
+else if(hour>13&&hour<=19)
+{
+    document.body.style.backgroundColor='blue';
+    alert('Allahverdi atanin balasi');
+}
+else if(hour>19&&hour<=23)
+{
+    document.body.style.backgroundColor='black';
+    alert('yerin rahatdi??');
+}
+else if(hour>0&&hour<=9)
+{
+    document.body.style.backgroundColor='gray';
+    alert('baxma birce nefere');
+}*/
+
+
+//task2
+/*
+var Kvadrat=12;
+console.log(Math.pow(Kvadrat,2));
+
+
+var rad =6;
+var sahe=Math.pow(rad,2)*Math.PI;
+console.log(sahe);
+*/
